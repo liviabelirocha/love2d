02e4d743c589f1bb72d55545918fda4ca076a70e
liviabelirocha <liviabelirocha@outlook.com>
2020-05-16 18:14:45 -0300

    pipes added

--- flappy_bird/main.lua
+++ flappy_bird/main.lua
@@ -2,6 +2,7 @@ push = require 'libs/push'
 Class = require 'libs/class'
 
 require 'Bird'
+require 'Pipe'
 
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
@@ -22,10 +23,16 @@ local BACKGROUND_LOOPING_POINT = 413
 
 local bird = Bird()
 
+local pipes = {}
+
+local spawnTimer = 0
+
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
 
     love.window.setTitle('Flappy Bird')
+
+    math.randomseed(os.time())
 
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         vsync = true,
@@ -59,7 +66,21 @@ function love.update(dt)
     backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt) % BACKGROUND_LOOPING_POINT
     groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt) % VIRTUAL_WIDTH
 
+    spawnTimer = spawnTimer + dt
+    if spawnTimer > 2 then
+        table.insert(pipes, Pipe())
+        spawnTimer = 0
+    end
+
     bird:update(dt)
+
+    for k, pipe in pairs(pipes) do
+        pipe:update(dt)
+        
+        if pipe.x < -pipe.width then
+            table.remove(pipes, k)
+        end
+    end
 
     love.keyboard.keysPressed = {}
 end
@@ -69,9 +90,15 @@ function love.draw()
     
     love.graphics.draw(background, -backgroundScroll, 0)
 
+    for k, pipe in pairs(pipes) do
+        pipe:render()
+    end
+
     love.graphics.draw(ground, -groundScroll, VIRTUAL_HEIGHT - 16)
 
     bird:render()
     
     push:finish()
 end
+
+--1796.475 31 1262.617
