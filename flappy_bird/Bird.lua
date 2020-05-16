825606dc8bca43212d9999ca57a76745d92a22f5
liviabelirocha <liviabelirocha@outlook.com>
2020-05-16 12:49:38 -0300

    jump

--- flappy_bird/Bird.lua
+++ flappy_bird/Bird.lua
@@ -20,5 +20,9 @@ end
 function Bird:update(dt)
     self.dy = self.dy + GRAVITY * dt
 
+    if love.keyboard.wasPressed('space') then
+        self.dy = -5
+    end
+
     self.y = self.y + self.dy
 end
