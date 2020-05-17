c92ef897b691a88119435fe22efb8596e9586d92
liviabelirocha <liviabelirocha@outlook.com>
2020-05-17 15:10:13 -0300

    added paddles

--- breakout/src/Dependencies.lua
+++ breakout/src/Dependencies.lua
@@ -3,7 +3,12 @@ Class = require 'libs/class'
 
 require 'src/constants'
 
+require 'src/Quad'
+
+require 'src/Paddle'
+
 require 'src/StateMachine'
 
 require 'src/states/BaseState'
 require 'src/states/StartState'
+require 'src/states/PlayState'
